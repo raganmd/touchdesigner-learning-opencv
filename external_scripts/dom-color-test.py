e66f2612a914e6a38b33736464a95d5b4a4e87f8
Matthew Ragan <matthew.ragan@obscuradigital.com>
2018-05-10 17:26:53 -0700

    Functional code for finding dominant color

--- external_scripts/dom-color-test.py
+++ external_scripts/dom-color-test.py
@@ -1,21 +1,24 @@
-import cv2
-import numpy
-import sklearn
+# import cv2
+# import numpy
+# import sklearn
 
-print("well, that worked... finally")
+# print("well, that worked... finally")
 
 
-# save out an image from touch to a temporary location
-colorImgTemp 			= "../cache/{file}.jpg"
-colorImgFilePath 		= colorImgTemp.format(file="color1")
+# # save out an image from touch to a temporary location
+# colorImgTemp 			= "../cache/{file}.jpg"
+# colorImgFilePath 		= colorImgTemp.format(file="color1")
 
-# open image with openCV
-img 					= cv2.imread(colorImgFilePath, 1)
+# # open image with openCV
+# img 					= cv2.imread(colorImgFilePath, 1)
+# # print(img)
+
+# # transform image from bgr to rgb
+# img 					= cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+# img                     = img.reshape((img.shape[0] * img.shape[1], 3))
+
 # print(img)
 
-# transform image from bgr to rgb
-img 					= cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-img                     = img.reshape((img.shape[0] * img.shape[1], 3))
-
-print(img)
+import osc
--- scripts/text_color_detection.py
+++ scripts/text_color_detection.py
@@ -1,69 +1,81 @@
-# creating a circle with openCV
+'''
+    finding dominant color with openCV
+
+    dependencies
+    > numpy
+    > scipy
+    > sklearn
+
+    references
+    > https://buzzrobot.com/dominant-colors-in-an-image-using-k-means-clustering-3c7af4622036
+    > https://gist.github.com/skt7/71044f42f9323daec3aa035cd050884e
+    > https://www.pyimagesearch.com/2014/05/26/opencv-python-k-means-color-clustering/
+'''
 
 # import libraries
 import sys
 import cv2
-import numpy
 
-readyToProcess      = False
+readyToProcess          = False
+
+mypath = "C:\\Program Files\\Python35"
+if mypath not in sys.path:
+    sys.path.append(mypath)
+
+try:
+    from sklearn.cluster import KMeans
+    readyToProcess      = True
+    print("Ready to Process")
+
+except:
+    print("Missing Library")
+
+
 
 colorImgTemp 			= "cache/{file}.jpg"
 colorImgFilePath 		= colorImgTemp.format(file="color1")
 imgFileTOP 				= op('null1')
-imgFileTOP.save( colorImgFilePath , async=False )
+clusters                = 10
+rampTableDAT            = op('ramp1_keys')
+
+if readyToProcess:
+    # save out an image from touch to a temporary location
+    imgFileTOP.save( colorImgFilePath , async=False )
+
+    # open image with openCV
+    img 					= cv2.imread(colorImgFilePath, 1)
+    # transform image from bgr to rgb
+    img 					= cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    # reshape the data
+    img                     = img.reshape(img.shape[0] * img.shape[1], 3)
+
+    kmeans                  = KMeans(n_clusters=clusters)
+
+    kmeans.fit(img)
+    colors                  = kmeans.cluster_centers_
+    labels                  = kmeans.labels_
+    ramp_colors             = colors.astype(int)
 
 
-
-# save out an image from touch to a temporary location
-
-# open image with openCV
-img 					= cv2.imread(colorImgFilePath, 1)
-# transform image from bgr to rgb
-img 					= cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-size 					= 1000000
-
-kmeansCluster 			= cv2.kmeans(img, 3, size, 2, 2)
+    new_ramp_colors = sorted(ramp_colors, key=lambda x: x[0])
 
 
+    rampTableDAT.clear()
+    header                  = ['pos', 'r', 'g', 'b', 'a']
+    rampTableDAT.appendRow(header)
 
+    for item in enumerate( new_ramp_colors ):
+        # normalize vals
+        colors_normalized   = [color_val / 255 for color_val in item[1]]
 
+        # calculate position val
+        pos                 = item[0] / (len(ramp_colors) - 1)
 
+        colors_normalized.insert(0, pos)
+        colors_normalized.append(1)
 
+        rampTableDAT.appendRow(colors_normalized)
 
-
-# # grab external image
-# imgFile 				= app.samplesFolder+'/Map/Banana.tif'
-# img						= cv2.imread(imgFile, 3)
-
-# # get the dimensions of the file
-# imgHeight, imgWidth 	= img.shape[:2]
-# print(imgWidth, imgHeight)
-
-# # variables for file locations
-# filePath 				= 'output/{file}.png'
-# fileName 				= "circle_banana_test"
-
-# # format the file path
-# filePath 				= filePath.format(file=fileName)
-
-# # find the center coordinates for drwaing the circle
-# circleX 				= int(imgWidth/2)
-# circleY 				= int(imgHeight/2)
-
-# circleRadius 			= 200
-
-# circleColor 			= (0,0,255)
-
-# circleThickness			= 5
-
-# # draw a circle with openCV
-# # color format is (Blue, Green, Red)
-# circle 					= cv2.circle( img, 
-# 									  (circleX, circleY), 
-# 									  circleRadius, 
-# 									  circleColor, 
-# 									  circleThickness)
-
-# # save the resulting image
-# cv2.imwrite(filePath, circle)
+else:
+    pass
